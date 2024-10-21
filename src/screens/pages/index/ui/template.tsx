1c02f17e19a737002384811f8179a3a65d7d6069
Danil Paiusov <94990460+jsswpt@users.noreply.github.com>
2024-10-21 15:44:56 +0500

    feat: implement 'AuthLayout' (#4)

--- src/screens/pages/index/ui/template.tsx
+++ src/screens/pages/index/ui/template.tsx
@@ -1,25 +1,8 @@
-import { Box } from "@mui/material";
-
-import { AppBar } from "root/widgets/app-bar";
-import { Sidebar } from "root/widgets/sidebar";
-
-import { APPBAR_HEIGHT, SIDEBAR_WIDTH } from "root/shared/lib/mui";
-
 type TemplateProps = {
   // TODO: удалить
   isFallback?: boolean;
 };
 
 export const Template = ({ isFallback }: TemplateProps) => (
-  <Box minHeight="100svh">
-    <AppBar />
-    <Sidebar />
-    <Box
-      padding={({ spacing }) => spacing(16)}
-      paddingLeft={({ spacing }) => spacing(SIDEBAR_WIDTH + 16)}
-      paddingTop={({ spacing }) => spacing(APPBAR_HEIGHT + 16)}
-    >
-      'IndexPage' template {isFallback ? "fallback" : "component"}
-    </Box>
-  </Box>
+  <>'IndexPage' template {isFallback ? "fallback" : "component"}</>
 );
