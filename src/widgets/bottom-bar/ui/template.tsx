dd076476e13a2a514f63e455f9ddd48a7b548796
jsswpt <paiusov.d@yandex.ru>
2024-10-22 09:03:06 +0500

    fix: remove unnecessary prop

--- src/widgets/bottom-bar/ui/template.tsx
+++ src/widgets/bottom-bar/ui/template.tsx
@@ -3,14 +3,12 @@ import { BottomNavigation, Box } from "@mui/material";
 type TemplateProps = {
   items: React.ReactNode;
   value?: string;
-  onChange?: (event: React.SyntheticEvent, value: string) => void;
 };
 
-export const Template = ({ items, onChange, value }: TemplateProps) => (
+export const Template = ({ items, value }: TemplateProps) => (
   <Box position="fixed" left={0} right={0} bottom={0}>
     <BottomNavigation
       value={value}
-      onChange={onChange}
       showLabels
       sx={{
         backdropFilter: "blur(8px)",
